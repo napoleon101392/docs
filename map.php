c86d71a07cb0a0efc2da2f1b47203b6d5dfba9be
Daison Carino <daison12006013@gmail.com>
2015-10-16 05:35:49 +0000

    adding map for future structure

---
+++ map.php
@@ -0,0 +1,56 @@
+<?php
+
+return [
+
+    'Setup' => [
+        'Installation'  => 'setup/Installation.md',
+        'Configuration' => 'setup/configuration.md',
+    ],
+
+    'MMVC' => [
+        'Module'        => 'mmvc/module.md',
+        'Model'         => 'mmvc/model.md',
+        'View'          => 'mmvc/view.md',
+        'Controller'    => 'mmvc/controller.md',
+    ],
+
+    'Supporting Structure' => [
+        'Routing'          => 'supporting_structure/routing.md',
+        'Error Handler'    => 'supporting_structure/error_handler.md',
+        'Service Provider' => 'supporting_structure/provider.md',
+        'Validator'        => 'supporting_structure/validator.md',
+    ],
+
+    'Services' => [
+        'Access Control Lists' => 'services/acl.md',
+        'Aliaser'              => 'services/aliaser.md',
+        'Auth'                 => 'services/auth.md',
+        'Cache'                => 'services/cache.md',
+        'DB'                   => 'services/db.md',
+        'Dispatcher'           => 'services/dispatcher.md',
+        'Filter'               => 'services/filter.md',
+        'Flash'                => 'services/flash.md',
+        'FlashBag'             => 'services/flash_bag.md',
+        'Lang'                 => 'services/lang.md',
+        'Log'                  => 'services/log.md',
+        'Mail'                 => 'services/mail.md',
+        // 'Metadata Adapter'  => 'services/metadata_adapter.md',
+        'Mongo'                => 'services/mongo.md',
+        'Redirect'             => 'services/redirect.md',
+        'Request'              => 'services/request.md',
+        'Response'             => 'services/response.md',
+        'Router'               => 'services/router.md',
+        'Session'              => 'services/session.md',
+        'URL'                  => 'services/url.md',
+        'View'                 => 'services/view.md',
+        'Flysystem'            => 'services/flysystem.md',
+    ],
+
+    'Misc.' => [
+        'Brood Console' => 'misc/console.md',
+        'Database' => [
+            'Migrations' => 'misc/database/migrations.md',
+            'Factories'  => 'misc/database/factories.md',
+        ],
+    ],
+];
